bda8dccbfd526544467caea960053eb0f976c8f1
Cyb0nix <cedric.yoganathan@efrei.net>
2024-11-27 14:52:24 +0100

    should_notify_twice_when_applying_discount_for_two_users_v1

--- java/src/test/java/info/dmerej/DiscountApplierTest.java
+++ java/src/test/java/info/dmerej/DiscountApplierTest.java
@@ -3,6 +3,15 @@ package info.dmerej;
 import org.junit.jupiter.api.Test;
 
 public class DiscountApplierTest {
+
+  static class NotifierClassV1 implements Notifier {
+    public int count = 0;
+    @Override
+    public void notify(User user, String message) {
+      count++;
+    }
+  }
+
   @Test
   void should_notify_twice_when_applying_discount_for_two_users_v1() {
     // TODO: write a test to demonstrate the bug in DiscountApplier.applyV1()
